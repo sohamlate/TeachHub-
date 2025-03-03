f3f108587085bd609e11f11243f96aa568c23f56
sohamlate <sohamslate2004@gmail.com>
2025-03-03 19:35:52 +0530

    frontend setup

--- New folder/routes/contactRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { submitContactForm } = require('../controllers/contactController');
-
-router.post('/', submitContactForm);
-
-module.exports = router;
